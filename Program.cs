0785d8b71e6d6b62fda0014516e0b86123ee8b6c
Björn Strömberg <bjorn.stromberg@codic.se>
2022-12-06 11:46:17 +0100

    dag 3

--- Program.cs
+++ Program.cs
@@ -65,37 +65,37 @@
 {
     var text = File.ReadAllText("input/day3.txt");
 
-    var plays = text
-        .Split("\n")
-        .Select(l => l.Split())
-        .Select(p => (
-            They: p[0][0] - 'A',
-            Us: p[1][0] - 'X'
-        ));
+    var rugsacks = text
+        .Split("\n");
 
-    var score = (int us, int they) => 1 + us + (us - they) switch
-    {
-        2 or -1 => 0,
-        0 => 3,
-        _ => 6,
-    };
+    var priority = (char c) => c switch
+        {
+            >= 'a' and <= 'z' => (c - 'a') + 1,
+            _ => (c - 'A') + 27,
+        };
 
-    var result_1 = plays
-        .Sum(p => score(p.Us, p.They));
-
-    var result_2 = plays
-        .Select(p => (
-            They: p.They,
-            Us: p.Us switch
-            {
-                0 => (p.They + 2) % 3,
-                1 => p.They,
-                _ => (p.They + 1) % 3
-            }
+    var result_1 = rugsacks
+        .Select(l => (
+            Left: l.Substring(0, l.Length / 2),
+            Right: l.Substring(l.Length / 2)
         ))
-        .Sum(p => score(p.Us, p.They));
+        .Select(r => r.Left.Intersect(r.Right).Single())
+        .Sum(priority);
 
-    Console.WriteLine($"Day 2 answers: {result_1}, {result_2}");
+    var result_2 = rugsacks
+        .Zip(Enumerable.Range(0, rugsacks.Count()).Select(n => n / 3))
+        .GroupBy(zip => zip.Second)
+        .Select(group => group
+            .Select(zip => zip.First.Trim()))
+        .Select(group => group
+            .Take(2)
+            .Aggregate(
+                group.Last().AsEnumerable(),
+                (a, b) => a.Intersect(b))
+            .Single())
+        .Sum(priority);
+
+    Console.WriteLine($"Day 3 answers: {result_1}, {result_2}");
 }
 
 static class IEnumerableExtensions
@@ -103,6 +103,12 @@ static class IEnumerableExtensions
     public static IEnumerable<T> Do<T>(this IEnumerable<T> enumerable, int count, Action<T> action)
     {
         foreach (var v in enumerable.Take(count))
+            action(v);
+        return enumerable;
+    }
+    public static IEnumerable<T> Do<T>(this IEnumerable<T> enumerable, Action<T> action)
+    {
+        foreach (var v in enumerable)
             action(v);
         return enumerable;
     }
